1bf631f4bb9c6c60ad97f837ecd82d31d1045d03
DANIEL AGUDELO MONTOYA <dagudelo483@misena.edu.co>
2022-07-25 13:55:44 -0500

    a

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PuebaNET6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
